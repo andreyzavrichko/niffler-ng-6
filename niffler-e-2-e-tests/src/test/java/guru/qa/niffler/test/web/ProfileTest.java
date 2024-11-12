8f91a498fda0f2779a20057539f38110fdad57f7
a.zavrichko <andrey@zavrichko.dev>
2024-11-12 14:09:07 +0200

    синхронизация

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/ProfileTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/ProfileTest.java
@@ -1,18 +1,16 @@
+
 package guru.qa.niffler.test.web;
 
-import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideDriver;
 import guru.qa.niffler.jupiter.annotation.Category;
-import guru.qa.niffler.jupiter.annotation.ScreenShotTest;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.jupiter.annotation.meta.WebTest;
 import guru.qa.niffler.model.rest.UserJson;
 import guru.qa.niffler.page.LoginPage;
 import guru.qa.niffler.page.MainPage;
 import guru.qa.niffler.page.ProfilePage;
+import guru.qa.niffler.utils.SelenideUtils;
 import org.junit.jupiter.api.Test;
-
-import java.awt.image.BufferedImage;
-import java.io.IOException;
 
 import static guru.qa.niffler.utils.RandomDataUtils.randomCategoryName;
 import static guru.qa.niffler.utils.RandomDataUtils.randomName;
@@ -20,6 +18,7 @@ import static guru.qa.niffler.utils.RandomDataUtils.randomName;
 @WebTest
 public class ProfileTest {
 
+    private final SelenideDriver driver = new SelenideDriver(SelenideUtils.chromeConfig);
     @User(
             categories = @Category(
                     archived = true
@@ -29,12 +28,12 @@ public class ProfileTest {
     void archivedCategoryShouldPresentInCategoriesList(UserJson user) {
         final String categoryName = user.testData().categoryDescriptions()[0];
 
-        Selenide.open(LoginPage.URL, LoginPage.class)
+        driver.open(LoginPage.URL, LoginPage.class)
                 .fillLoginPage(user.username(), user.testData().password())
                 .submit(new MainPage())
                 .checkThatPageLoaded();
 
-        Selenide.open(ProfilePage.URL, ProfilePage.class)
+        driver.open(ProfilePage.URL, ProfilePage.class)
                 .checkArchivedCategoryExists(categoryName);
     }
 
@@ -47,12 +46,12 @@ public class ProfileTest {
     void activeCategoryShouldPresentInCategoriesList(UserJson user) {
         final String categoryName = user.testData().categoryDescriptions()[0];
 
-        Selenide.open(LoginPage.URL, LoginPage.class)
+        driver.open(LoginPage.URL, LoginPage.class)
                 .fillLoginPage(user.username(), user.testData().password())
                 .submit(new MainPage())
                 .checkThatPageLoaded();
 
-        Selenide.open(ProfilePage.URL, ProfilePage.class)
+        driver.open(ProfilePage.URL, ProfilePage.class)
                 .checkCategoryExists(categoryName);
     }
 
@@ -61,18 +60,18 @@ public class ProfileTest {
     void shouldUpdateProfileWithAllFieldsSet(UserJson user) {
         final String newName = randomName();
 
-        ProfilePage profilePage = Selenide.open(LoginPage.URL, LoginPage.class)
+        ProfilePage profilePage = driver.open(LoginPage.URL, LoginPage.class)
                 .fillLoginPage(user.username(), user.testData().password())
                 .submit(new MainPage())
                 .checkThatPageLoaded()
                 .getHeader()
                 .toProfilePage()
-                .uploadPhotoFromClasspath("img/cat.png")
+                .uploadPhotoFromClasspath("img/cat.jpeg")
                 .setName(newName)
                 .submitProfile()
                 .checkAlertMessage("Profile successfully updated");
 
-        Selenide.refresh();
+        driver.refresh();
 
         profilePage.checkName(newName)
                 .checkPhotoExist();
@@ -83,7 +82,7 @@ public class ProfileTest {
     void shouldUpdateProfileWithOnlyRequiredFields(UserJson user) {
         final String newName = randomName();
 
-        ProfilePage profilePage = Selenide.open(LoginPage.URL, LoginPage.class)
+        ProfilePage profilePage = driver.open(LoginPage.URL, LoginPage.class)
                 .fillLoginPage(user.username(), user.testData().password())
                 .submit(new MainPage())
                 .checkThatPageLoaded()
@@ -93,7 +92,7 @@ public class ProfileTest {
                 .submitProfile()
                 .checkAlertMessage("Profile successfully updated");
 
-        Selenide.refresh();
+        driver.refresh();
 
         profilePage.checkName(newName);
     }
@@ -103,7 +102,7 @@ public class ProfileTest {
     void shouldAddNewCategory(UserJson user) {
         String newCategory = randomCategoryName();
 
-        Selenide.open(LoginPage.URL, LoginPage.class)
+        driver.open(LoginPage.URL, LoginPage.class)
                 .fillLoginPage(user.username(), user.testData().password())
                 .submit(new MainPage())
                 .checkThatPageLoaded()
@@ -128,26 +127,12 @@ public class ProfileTest {
     )
     @Test
     void shouldForbidAddingMoreThat8Categories(UserJson user) {
-        Selenide.open(LoginPage.URL, LoginPage.class)
+        driver.open(LoginPage.URL, LoginPage.class)
                 .fillLoginPage(user.username(), user.testData().password())
                 .submit(new MainPage())
                 .checkThatPageLoaded()
                 .getHeader()
                 .toProfilePage()
                 .checkThatCategoryInputDisabled();
-    }
-
-    @User
-    @ScreenShotTest(value = "img/profile-expected.png")
-    void checkProfileImageTest(UserJson user, BufferedImage expectedProfileImage) throws IOException {
-        Selenide.open(LoginPage.URL, LoginPage.class)
-                .fillLoginPage(user.username(), user.testData().password())
-                .submit(new MainPage())
-                .checkThatPageLoaded()
-                .getHeader()
-                .toProfilePage()
-                .uploadPhotoFromClasspath("img/cat.png")
-                .submitProfile()
-                .checkProfileImage(expectedProfileImage);
     }
 }
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingWebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingWebTest.java
@@ -1,31 +1,31 @@
+
 package guru.qa.niffler.test.web;
 
-import com.codeborne.selenide.Selenide;
-import guru.qa.niffler.condition.Bubble;
-import guru.qa.niffler.condition.Color;
-import guru.qa.niffler.jupiter.annotation.Category;
+import com.codeborne.selenide.SelenideDriver;
 import guru.qa.niffler.jupiter.annotation.ScreenShotTest;
 import guru.qa.niffler.jupiter.annotation.Spending;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.jupiter.annotation.meta.WebTest;
-import guru.qa.niffler.model.rest.SpendJson;
 import guru.qa.niffler.model.rest.UserJson;
 import guru.qa.niffler.page.LoginPage;
 import guru.qa.niffler.page.MainPage;
-import guru.qa.niffler.page.component.SpendingTable;
 import guru.qa.niffler.page.component.StatComponent;
 import guru.qa.niffler.utils.RandomDataUtils;
 import guru.qa.niffler.utils.ScreenDiffResult;
+import guru.qa.niffler.utils.SelenideUtils;
 import org.junit.jupiter.api.Test;
+
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.util.Date;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @WebTest
 public class SpendingWebTest {
+
+    private final SelenideDriver driver = new SelenideDriver(SelenideUtils.chromeConfig);
+
     @User(
             spendings = @Spending(
                     category = "Обучение",
@@ -36,16 +36,19 @@ public class SpendingWebTest {
     @Test
     void categoryDescriptionShouldBeChangedFromTable(UserJson user) {
         final String newDescription = "Обучение Niffler Next Generation";
-        Selenide.open(LoginPage.URL, LoginPage.class)
+
+        driver.open(LoginPage.URL, LoginPage.class)
                 .fillLoginPage(user.username(), user.testData().password())
                 .submit(new MainPage())
                 .getSpendingTable()
                 .editSpending("Обучение Advanced 2.0")
                 .setNewSpendingDescription(newDescription)
                 .saveSpending();
+
         new MainPage().getSpendingTable()
                 .checkTableContains(newDescription);
     }
+
     @User
     @Test
     void shouldAddNewSpending(UserJson user) {
@@ -53,7 +56,8 @@ public class SpendingWebTest {
         int amount = 100;
         Date currentDate = new Date();
         String description = RandomDataUtils.randomSentence(3);
-        Selenide.open(LoginPage.URL, LoginPage.class)
+
+        driver.open(LoginPage.URL, LoginPage.class)
                 .fillLoginPage(user.username(), user.testData().password())
                 .submit(new MainPage())
                 .getHeader()
@@ -64,13 +68,15 @@ public class SpendingWebTest {
                 .setNewSpendingDescription(description)
                 .saveSpending()
                 .checkAlertMessage("New spending is successfully created");
+
         new MainPage().getSpendingTable()
                 .checkTableContains(description);
     }
+
     @User
     @Test
     void shouldNotAddSpendingWithEmptyCategory(UserJson user) {
-        Selenide.open(LoginPage.URL, LoginPage.class)
+        driver.open(LoginPage.URL, LoginPage.class)
                 .fillLoginPage(user.username(), user.testData().password())
                 .submit(new MainPage())
                 .getHeader()
@@ -80,10 +86,11 @@ public class SpendingWebTest {
                 .saveSpending()
                 .checkFormErrorMessage("Please choose category");
     }
+
     @User
     @Test
     void shouldNotAddSpendingWithEmptyAmount(UserJson user) {
-        Selenide.open(LoginPage.URL, LoginPage.class)
+        driver.open(LoginPage.URL, LoginPage.class)
                 .fillLoginPage(user.username(), user.testData().password())
                 .submit(new MainPage())
                 .getHeader()
@@ -93,6 +100,7 @@ public class SpendingWebTest {
                 .saveSpending()
                 .checkFormErrorMessage("Amount has to be not less then 0.01");
     }
+
     @User(
             spendings = @Spending(
                     category = "Обучение",
@@ -102,13 +110,14 @@ public class SpendingWebTest {
     )
     @Test
     void deleteSpendingTest(UserJson user) {
-        Selenide.open(LoginPage.URL, LoginPage.class)
+        driver.open(LoginPage.URL, LoginPage.class)
                 .fillLoginPage(user.username(), user.testData().password())
                 .submit(new MainPage())
                 .getSpendingTable()
                 .deleteSpending("Обучение Advanced 2.0")
                 .checkTableSize(0);
     }
+
     @User(
             spendings = @Spending(
                     category = "Обучение",
@@ -116,124 +125,20 @@ public class SpendingWebTest {
                     amount = 79990
             )
     )
-    @ScreenShotTest(value = "img/expected-stat.png")
+    @ScreenShotTest("img/expected-stat.png")
     void checkStatComponentTest(UserJson user, BufferedImage expected) throws IOException, InterruptedException {
-        StatComponent statComponent = Selenide.open(LoginPage.URL, LoginPage.class)
+        StatComponent statComponent = driver.open(LoginPage.URL, LoginPage.class)
                 .fillLoginPage(user.username(), user.testData().password())
                 .submit(new MainPage())
                 .getStatComponent();
+
         Thread.sleep(3000);
+
         assertFalse(new ScreenDiffResult(
                 expected,
                 statComponent.chartScreenshot()
         ), "Screen comparison failure");
-        statComponent.checkBubblesColor(Color.yellow);
-    }
 
-    @User(
-            spendings = @Spending(
-                    category = "Обучение",
-                    description = "Обучение Advanced 2.0",
-                    amount = 79990
-            )
-    )
-    @Test
-    void checkStatBubbleContent(UserJson user) throws InterruptedException {
-        StatComponent statComponent = Selenide.open(LoginPage.URL, LoginPage.class)
-                .fillLoginPage(user.username(), user.testData().password())
-                .submit(new MainPage())
-                .getStatComponent();
-        Thread.sleep(3000);
-        Bubble bubble = new Bubble(Color.yellow, "Обучение 79990 ₽");
-        statComponent.checkBubbles(bubble);
-    }
-
-    @User(
-            categories = {
-                    @Category(name = "Обучение"),
-                    @Category(name = "Развлечения")
-            },
-            spendings = {
-                    @Spending(
-                            category = "Обучение",
-                            description = "Обучение Advanced 2.0",
-                            amount = 1000
-                    ),
-                    @Spending(
-                            category = "Развлечения",
-                            description = "Поход в кино",
-                            amount = 100
-                    )
-            }
-    )
-    @Test
-    void checkStatBubblesInAnyOrder(UserJson user) throws InterruptedException {
-        StatComponent statComponent = Selenide.open(LoginPage.URL, LoginPage.class)
-                .fillLoginPage(user.username(), user.testData().password())
-                .submit(new MainPage())
-                .getStatComponent();
-        Thread.sleep(3000);
-        Bubble bubble1 = new Bubble(Color.yellow, "Обучение 1000 ₽");
-        Bubble bubble2 = new Bubble(Color.green, "Развлечения 100 ₽");
-        statComponent.checkBubblesInAnyOrder(bubble2, bubble1);
-    }
-
-    @User(
-            categories = {
-                    @Category(name = "Обучение"),
-                    @Category(name = "Развлечения")
-            },
-            spendings = {
-                    @Spending(
-                            category = "Обучение",
-                            description = "Обучение Advanced 2.0",
-                            amount = 1000
-                    ),
-                    @Spending(
-                            category = "Развлечения",
-                            description = "Поход в кино",
-                            amount = 100
-                    )
-            }
-    )
-    @Test
-    void checkStatBubbleContainsAmongOtherBubbles(UserJson user) throws InterruptedException {
-        StatComponent statComponent = Selenide.open(LoginPage.URL, LoginPage.class)
-                .fillLoginPage(user.username(), user.testData().password())
-                .submit(new MainPage())
-                .getStatComponent();
-        Thread.sleep(3000);
-        Bubble bubble = new Bubble(Color.yellow, "Обучение 1000 ₽");
-        statComponent.checkBubblesContains(bubble);
-    }
-
-    @User(
-            categories = {
-                    @Category(name = "Обучение"),
-                    @Category(name = "Развлечения")
-            },
-            spendings = {
-                    @Spending(
-                            category = "Обучение",
-                            description = "Обучение Advanced 2.0",
-                            amount = 1000
-                    ),
-                    @Spending(
-                            category = "Развлечения",
-                            description = "Поход в кино",
-                            amount = 100
-                    )
-            }
-    )
-    @Test
-    void checkSpendsExistInTable(UserJson user) throws InterruptedException {
-        SpendingTable spendingTable = Selenide.open(LoginPage.URL, LoginPage.class)
-                .fillLoginPage(user.username(), user.testData().password())
-                .submit(new MainPage())
-                .getSpendingTable();
-        Thread.sleep(3000);
-        // Извлекаем список SpendJson и передаем его в checkSpendingTable
-        List<SpendJson> expectedSpends = user.testData().spends();
-        spendingTable.checkSpendingTable(expectedSpends.toArray(new SpendJson[0]));
+        statComponent.checkBubbles();
     }
 }
