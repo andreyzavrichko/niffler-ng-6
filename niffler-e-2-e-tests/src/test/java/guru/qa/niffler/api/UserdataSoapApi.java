55df8fe98e75089b098b099e3a8d6031b0745f3f
a.zavrichko <andrey@zavrichko.dev>
2025-01-31 20:04:46 +0200

    ДЗ-27

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/UserdataSoapApi.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/UserdataSoapApi.java
@@ -1,23 +1,52 @@
 package guru.qa.niffler.api;
-import guru.qa.jaxb.userdata.AllUsersRequest;
-import guru.qa.jaxb.userdata.CurrentUserRequest;
-import guru.qa.jaxb.userdata.UserResponse;
-import guru.qa.jaxb.userdata.UsersResponse;
+
+import guru.qa.jaxb.userdata.*;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
+
 public interface UserdataSoapApi {
-    @Headers(value = {
-            "Content-type: text/xml",
+
+    @Headers({
+            "Content-Type: text/xml",
             "Accept-Charset: utf-8"
     })
     @POST("ws")
-    Call<UserResponse> currentUser(@Body CurrentUserRequest currentUserRequest);
-    @Headers(value = {
-            "Content-type: text/xml",
+    Call<UserResponse> getUser(@Body CurrentUserRequest request);
+
+    @Headers({
+            "Content-Type: text/xml",
             "Accept-Charset: utf-8"
     })
     @POST("ws")
-    Call<UsersResponse> allUsers(@Body AllUsersRequest allUsersRequest);
+    Call<UsersResponse> fetchFriends(@Body FriendsPageRequest request);
+
+    @Headers({
+            "Content-Type: text/xml",
+            "Accept-Charset: utf-8"
+    })
+    @POST("ws")
+    Call<UserResponse> initiateFriendRequest(@Body SendInvitationRequest request);
+
+    @Headers({
+            "Content-Type: text/xml",
+            "Accept-Charset: utf-8"
+    })
+    @POST("ws")
+    Call<UserResponse> approveRequest(@Body AcceptInvitationRequest request);
+
+    @Headers({
+            "Content-Type: text/xml",
+            "Accept-Charset: utf-8"
+    })
+    @POST("ws")
+    Call<UserResponse> refuseInvitation(@Body DeclineInvitationRequest request);
+
+    @Headers({
+            "Content-Type: text/xml",
+            "Accept-Charset: utf-8"
+    })
+    @POST("ws")
+    Call<Void> deleteFriendship(@Body RemoveFriendRequest request);
 }
